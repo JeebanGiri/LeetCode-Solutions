de938889e229e7c6c431e4756e6cc1b8f6048cb8
Jeeban <jeebangiri942@gmail.com>
2023-09-17 07:08:40 +0545

    update threeSum file

--- JavaScripts/threeSum.js
+++ JavaScripts/threeSum.js
@@ -2,21 +2,19 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+const number = [];
 let nums = [-1, 0, 1, 2, -1, -4];
 var threeSum = function (nums) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       for (let k = j + 1; k < nums.length; k++) {
-        if (i != j && i != k && j != k) {
-          if (nums[i] + nums[j] + nums[k] == 0) {
-            console.log(i,j,k);
-            return [i, j, k];
-          }
+        if (i != j && i != k && j != k && nums[i] + nums[j] + nums[k] == 0) {
+          number.push([nums[i], nums[j], nums[k]]);
         }
       }
     }
+    return number;
   }
 };
 
-// let out = threeSum(nums);h
-
+console.log(threeSum(nums));
