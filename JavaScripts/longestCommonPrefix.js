b3fe7698e86d08968e1299e084fa039690b225e4
Jeeban Giri <jeebangiri942@gmail.com>
2024-09-21 21:01:02 +0545

    add a prefix

--- JavaScripts/longestCommonPrefix.js
+++ JavaScripts/longestCommonPrefix.js
@@ -1,7 +1,10 @@
 const longestCommonPrefix = (strs) => {
+  let prefixs = "";
   for (let prefix in strs) {
     for (let i = 0; i < prefix.length; i++) {
-      console.log("dakhdf");
+      console.log(prefix[i]);
     }
   }
 };
+strs = ["flower", "flow", "flight"];
+console.log(longestCommonPrefix(strs));
