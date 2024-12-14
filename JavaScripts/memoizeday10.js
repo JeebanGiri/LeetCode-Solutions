a5c913fbb266cb9f32b8c7c0a6231f19369d226f
Jeeban Giri <jeebangiri942@gmail.com>
2024-12-14 18:01:53 +0545

    complete memoize function

--- JavaScripts/memoizeday10.js
+++ JavaScripts/memoizeday10.js
@@ -5,10 +5,24 @@ function memoize(fn) {
 
     if (cache.has(key)) {
       return cache.get(key);
-
-      const result = fn(...args);
-      cache.set(key, result);
-      return result;
     }
+
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
   };
 }
+
+const memoize = (fn) => {
+  const cache = {}; // Initialize Object for cache store
+  return function (...args) {
+    const key = JSON.stringify(args);
+
+    if (key in cache) {
+      return cache[key];
+    }
+    const result = fn(...args);
+    cache[key] = result; // set the cache key with it's value
+    return result;
+  };
+};
