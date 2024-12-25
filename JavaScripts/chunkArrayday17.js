ddc252874255947ef8580704c25907a4b119e770
Jeeban Giri <jeebangiri942@gmail.com>
2024-12-25 19:24:38 +0545

    add for loop for chunk array

--- JavaScripts/chunkArrayday17.js
+++ JavaScripts/chunkArrayday17.js
@@ -1,6 +1,9 @@
 var chunk = function (arr, size) {
+  const chunks = [];
+  for (let i = 0; i < arr.length; i += size) {
     
+  }
 };
 const arr = [1, 9, 6, 3, 2];
 const size = 3;
-console.log(chunk(arr,size));
+console.log(chunk(arr, size));
