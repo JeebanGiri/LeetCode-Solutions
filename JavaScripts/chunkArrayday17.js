3b33868e98fec01a8ab12567326bb90a821062dd
Jeeban Giri <jeebangiri942@gmail.com>
2024-12-25 19:29:09 +0545

    complete chunk array

--- JavaScripts/chunkArrayday17.js
+++ JavaScripts/chunkArrayday17.js
@@ -1,8 +1,9 @@
 var chunk = function (arr, size) {
   const chunks = [];
   for (let i = 0; i < arr.length; i += size) {
-    
+    chunks.push(arr.slice(i, i + size));
   }
+  return chunk;
 };
 const arr = [1, 9, 6, 3, 2];
 const size = 3;
